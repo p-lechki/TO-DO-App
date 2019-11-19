ac1e5b6f46cdd16972f3a5725a42d81cd5f5d32a
Paweł Lechki <pawel@iMac-admin.local>
2019-11-19 20:02:20 +0100

    TASK 13.1 done

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import Creator from '../Creator/Creator';
 import { settings } from '../../data/dataStore';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
+import Container from '../Container/Container';
 
 class List extends React.Component {
     static propTypes = {
@@ -21,20 +22,22 @@ class List extends React.Component {
     render() {
       const {columns, title, image, description, addColumn} = this.props;
       return (
-        <section className={styles.component}>
-          <Hero titleText={title} image={image} />
-          <div className={styles.description}>
-            {ReactHtmlParser(description)}
-          </div>
-          <div className={styles.columns}>
-            {columns.map(columnData => (
-                <Column key={columnData.id}{...columnData} />
-            ))}
-          </div>
-          <div className={styles.creator}>
-            <Creator text={settings.columnCreatorText} action={addColumn} />
-          </div>
-        </section>
+        <Container>
+          <section className={styles.component}>
+            <Hero titleText={title} image={image} />
+            <div className={styles.description}>
+              {ReactHtmlParser(description)}
+            </div>
+            <div className={styles.columns}>
+              {columns.map(columnData => (
+                  <Column key={columnData.id}{...columnData} />
+              ))}
+            </div>
+            <div className={styles.creator}>
+              <Creator text={settings.columnCreatorText} action={addColumn} />
+            </div>
+          </section>
+        </Container>
       );
     }
 }
